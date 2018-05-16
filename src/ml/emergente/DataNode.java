0af2893ead8785f0c05ec9aaca503672a14cb155
Jose Luis Raffalli <raffallijoseluis@gmail.com>
2018-05-16 17:54:04 -0400

    Data node again

--- src/ml/emergente/DataNode.java
+++ src/ml/emergente/DataNode.java
@@ -10,5 +10,37 @@ package ml.emergente;
  * @author Jose
  */
 public class DataNode {
+    // Atributos
+    float[] input;
+    int output;
+    
+    public DataNode(float inputValues[],int outputVal) {
+        this.input = inputValues;
+        this.output = outputVal;
+    }
+    public DataNode(int outputVal) {
+        this.output = outputVal;
+    }
+    public DataNode() {
+        this.input = new float[5];
+    }
+
+    public float[] getInput() {
+        return input;
+    }
+
+    public void setInput(float[] input) {
+        this.input = input;
+    }
+
+    public int getOutput() {
+        return output;
+    }
+
+    public void setOutput(int output) {
+        this.output = output;
+    }
+    
+    
     
 }
